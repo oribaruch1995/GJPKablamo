9d9cd854cff6fd67cb8515a45d0ebc2165f28cb1
barman100 <80104862+barman100@users.noreply.github.com>
2022-10-15 19:40:01 +0300

    enemy attack range and bullet logic
    enemy have sight range to start  follow player and attack range to start attack i added aim logic for the bullet

---
+++ Assets/Scripts/BulletAI.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletAI : MonoBehaviour
+{
+    public float Speed = 7f;
+    public GameObject TargetPlayer;
+    private Rigidbody _rb;
+    private Vector3 _moveDirection;
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+        TargetPlayer = GameObject.Find("Player");
+        _moveDirection = (TargetPlayer.transform.position - transform.position).normalized * Speed;
+        _rb.velocity = new Vector3(_moveDirection.x, 0, _moveDirection.z);
+        Destroy(gameObject, 4f);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name.Equals("Player"))
+        {
+            Debug.Log("hit player");
+            Destroy(gameObject);
+        }
+        else
+            Destroy(gameObject);
+    }
+}
