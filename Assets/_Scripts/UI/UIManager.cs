43fa80fe37f4608c2e5036d4d36b916215ea5460
barman100 <80104862+barman100@users.noreply.github.com>
2022-10-16 18:45:30 +0300

    UI Manager completed

--- Assets/_Scripts/UI/UIManager.cs
+++ Assets/_Scripts/UI/UIManager.cs
@@ -1,51 +1,102 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class UIManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float TempPlayerHP = 100; // temp until player health assigned
+    public int TempPlayerScore = 0; // temp until player health assigned
+    public int TempPlayerBullets = 100; // temp until player health assigned
+    public TextMeshProUGUI ScoreText;// assigned in inspetor
+    public TextMeshProUGUI BulletsText;// assigned in inspetor
+    public Image HpBarSlider; // assigned in inspetor
+    public GameObject BulletImageGO; // assigned in inspetor
+    public GameObject ReloadImageGO; // assigned in inspetor
+    public GameObject MuteImageGO; // assigned in inspetor
+    public GameObject UnMuteImageGO; // assigned in inspetor
+    public GameObject StartScreenGO; // assigned in inspetor
+    public GameObject GameHUDGO; // assigned in inspetor
+    public GameObject PauseScreenGO; // assigned in inspetor
+    public float CurrentHealth;
+    public float CurrentScore = 0;
+    private float _maxHealth = 100f;
 
-    // Update is called once per frame
     void Update()
     {
-        
+        HpBar();
+        BulletsStack();
+        Score();
     }
 
-    public void Play()
+    public void StartPlay()
     {
-        // hide canvas panel 
-        // hide play button
-        // turn HUD on (Life,Score,Bullets)
         // change to camera above player
+        StartScreenGO.SetActive(false);
+        GameHUDGO.SetActive(true);
     }
     public void HpBar()
     {
-        // create a slider to change hp depends on life
+        CurrentHealth = TempPlayerHP; // change temp player to HP amount from character
+        HpBarSlider.fillAmount = CurrentHealth / _maxHealth;
+    }
+    public void Score()
+    {
+        if (TempPlayerScore != CurrentScore) // change temp player to Score from character
+        {
+            CurrentScore = TempPlayerScore;
+            ScoreText.text = TempPlayerScore.ToString();
+        }
     }
     public void BulletsStack()
     {
-        // change bullet number depends on the magazine size
-        // call reload function when empty
+        BulletsText.text = TempPlayerBullets.ToString(); // change temp player to bullet amount from character
+        if (TempPlayerBullets <= 0)
+            NeedToReload();
     }
-    public void Reload()
+    public void NeedToReload()
     {
-        //change bullet logo to reload bullets
-        //on reload change it back to normal
+        BulletImageGO.SetActive(false);
+        ReloadImageGO.SetActive(true);
+    }
+    public void Reloaded() // assign to a key/button you reload
+    {
+        BulletImageGO.SetActive(true);
+        ReloadImageGO.SetActive(false);
     }
     public void Mute()
     {
-       // mute sound
-       // change logo to muted
+        // mute sound
+        MuteImageGO.SetActive(false);
+        UnMuteImageGO.SetActive(true);
     }
     public void UnMure()
     {
         // unmute sound
-        // change logo to unmuted
+        MuteImageGO.SetActive(true);
+        UnMuteImageGO.SetActive(false);
+    }
+    public void PauseBtn()
+    {
+        GameHUDGO.SetActive(false);
+        PauseScreenGO.SetActive(true);
+    }
+    public void ContinueBtn()
+    {
+        PauseScreenGO.SetActive(false);
+        GameHUDGO.SetActive(true);
+
+    }
+    public void ExitBtn()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+
+    }
+    public void ReplayBtn()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex); // change that  it will start from the beggining on the game
     }
 
 }
